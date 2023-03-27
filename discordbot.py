3916652d196e04b6fe8e751c4fe7fb9762419bab
lemonn9805 <104042721+lemonn9805@users.noreply.github.com>
2023-03-27 15:46:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -26,7 +26,7 @@ async def on_message(message):
         await message.channel.send('Hello!')
         
     if message.content.startswith(''):
-        await message.channel.send("{}아, 안녕".format(message.author.mention))
+        await message.channel.send("+play {}".format(message.author.mention))
 
 
 try:
